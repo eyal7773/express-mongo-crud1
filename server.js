317f70fc0f719ab5f5882eee877c7bcfb1f1adb6
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-19 09:10:01 +0300

    Insert form + Post route.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.post('/quotes',(req,res) => {
+    console.log('POST request received');
+})
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
