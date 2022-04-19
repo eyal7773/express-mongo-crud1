3564ded8cc92a97417bc872a25231b02be1a430f
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-19 09:40:36 +0300

    connect to specific db + get the db variable by the right way

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-
 async function connectToDb() {
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017');
         console.log('Connected to MongoDB server');
+        let db = client.db('quotes')
+        console.log('Connected to `quotes` database');
+        return db;
     } catch (error) {
         console.error('Unable to connect to MongoDB server, Error is: ', error);
+        return null;
     }
 }
         
-connectToDb();
+const db = connectToDb();
+console.log('db', db);
+// הדרך הפשוטה לעשות זאת היא לאחסן את כל הקריאות בתוך הקולבייק של יצירת החיבור למונגו
+// הבעיה היא שזה יוצר מה שנקרא 
+// callback hell
+// ולכן עדיף להחזיר את החיבור מתוך פונקציה או מחלקה
+// כמו שאנחנו עשינו
+// צריך לדעת שפעמים רבות, בעולם הגאווהסקריפט, אנחנו נראה עדין שימוש רב בקולבייקים
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
