a6c3d1fed07526aa03457ecc4c951d477005e035
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-20 10:48:09 +0300

    JSON response to PUT request

--- public/main.js
+++ public/main.js
@@ -4,12 +4,21 @@ const achavQuote = {
     quote:'אל יתהלל חוגר כמפתח',
 };
 
-updateBtn.addEventListener('click', () => {
-    fetch('/quotes',
+updateBtn.addEventListener('click', async () => {
+   let response = await fetch('/quotes',
             {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(achavQuote)
             }
     );
+
+    let responseJson = await response.json();
+    console.log('response is ', responseJson);
+    /** 
+     * בשלב זה אנחנו מתמקדים בלימוד של מונגו
+     * ולכן נעזוב את העדכון של הדום.
+     * אך בכל מקרה זה משהו שמוכר לכם כבר מהעבר
+     * 
+     */
 })
