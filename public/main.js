e090548988c7b6663ca13ccd1846f87c80312885
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-20 10:18:59 +0300

    prepare the PUT request

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-console.log('hello from main.js');
+const updateBtn = document.querySelector('#updateBtn');
+const achavQuote = {
+    name:'אחאב',
+    quote:'אל יתהלל חוגר כמפתח',
+};
+
+updateBtn.addEventListener('click', () => {
+    fetch('/quotes',
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(achavQuote)
+            }
+    );
+})
