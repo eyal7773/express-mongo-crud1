ef235d8ed300f6d1d5f705f3caf0b7b095fca89a
Eyal Bardugo <taliandeyal@gmail.com>
2022-04-20 09:18:43 +0300

    convert to ES6  modules (.mjs) , and solve relevant issues.

---
+++ server.mjs
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient }  from 'mongodb';
+
+// Based on: https://stackoverflow.com/a/62892482/1598814
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const app = express();
+
+
+
+async function connectToDb() {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017');
+        console.log('Connected to MongoDB server');
+        let db = await client.db('quotes')
+        console.log('Connected to `quotes` database');
+        return db;
+    } catch (error) {
+        console.error('Unable to connect to MongoDB server, Error is: ', error);
+        return null;
+    }
+}
+        
+    const db = await connectToDb();
+    const idiomsCollection = db.collection('idioms');
+    
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
+    app.get('/', (req,res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
+
+    app.post('/quotes',(req,res) => {
+        console.log(req.body);
+    })
+
+
+    app.listen(3000, () => {
+        console.log('Server is listening on port 3000');
+    })
+
+
